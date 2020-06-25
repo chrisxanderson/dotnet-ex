f54a1df8c5f30f0606aa6b8a56a0083115925636
Christopher Anderson <chris@anotheranderson.com>
2020-06-25 16:11:47 -0700

    A little farther along.

--- cs8dotnetcore3/Chapter11/WorkingWithEFCore/Program.cs
+++ cs8dotnetcore3/Chapter11/WorkingWithEFCore/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace WorkingWithEFCore
 {
@@ -6,7 +10,59 @@ namespace WorkingWithEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //QueryingCategories();
+            QueryingProducts();
+        }
+
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Categories and how many products they have:");
+
+                // a query to get all cateories and their related products
+                IQueryable<Category> cats = db.Categories
+                    .Include(c => c.Products);
+
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count } products.");
+                }
+            }
+        }
+
+        static void QueryingProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Products that cost more than a price, highest at top.");
+                string input;
+                decimal price;
+                do
+                {
+                    Write("Enter a product price: ");
+                    input = ReadLine();
+                } while (!decimal.TryParse(input, out price));
+
+                // Not working on Windows 10 - code hangs at ... highest at top.
+                /*
+                IQueryable<Product> prods = db.Products
+                    .Where(product => product.Cost > price)
+                    .OrderByDescending(product => product.Cost);
+                */
+
+                // alternative "fix"
+                IOrderedEnumerable<Product> prods = db.Products
+                    .AsEnumerable() // force client-side execution
+                    .Where(product => product.Cost > price)
+                    .OrderByDescending(product => product.Cost);
+                
+                foreach (Product item in prods)
+                {
+                    WriteLine("{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
+                    item.ProductID, item.ProductName, item.Cost, item.Stock);
+                }
+            }
         }
     }
 }
