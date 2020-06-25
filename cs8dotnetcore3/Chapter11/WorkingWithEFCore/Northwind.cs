37ef7cd7ba531b747fbac1bd6dcf35ed9b83d61a
chrisxanderson <chris@anotheranderson.com>
2020-06-25 10:32:35 -0700

    Initial commmit of cs8 book examples and Chapter 11 stuff.

---
+++ cs8dotnetcore3/Chapter11/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Packt.Shared
+{
+    // this manages the connection to the database
+    public class Northwind : DbContext
+    {
+        // these properties map to tables in the database
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
+
+            optionBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired() // NOT NULL
+            .HasMaxLength(15);
+        }
+    }
+}
